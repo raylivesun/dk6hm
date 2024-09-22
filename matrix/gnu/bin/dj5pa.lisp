003a7d381088325c26a88d30566bd2551c198915
raylivesun <162396999+raylivesun@users.noreply.github.com>
2024-09-22 04:08:22 -0300

    The provided code snippet is a C program that reverses the order of words in a given string and also reverses each word within the string. The main function prompts the user to enter a string, calls the reverseWords function to reverse the words, and then prints the reversed string.

---
+++ matrix/gnu/bin/dj5pa.lisp
@@ -0,0 +1,82 @@
+;; name project dj5pa
+;; name program dj
+;; name extension newlisp
+;; name data 5
+;; name unit 5
+
+;; name list complete type method social local
+;; style color fonts newlisp
+
+(set 'vector-matrix-table '("a" 97 "b" 98 "c" 99))
+
+;; check list loop form to right to left
+(for (c (char "a") (char "z"))
+     (push (list (char c) c) vector-matrix-table +1)) 
+
+;; list table sorted method link table in wallet
+(set 'table-sorted
+'(("table1" 0.382 0.06 0.387 0.241 7.00 0.206 58.6 0)
+  ("table2" 0.949 0.82 0.72 0.615 3.39 0.0068 -243 0)
+  ("table3" 1.00 1.00 1.00 1.00 0.00 0.0167 1.00 1)
+  ("table4" 0.53 0.11 1.52 1.88 1.85 0.0934 1.03 2)
+  ("table5" 11.2 318 5.20 11.86 1.31 0.0484 0.414 63)
+  ("table6" 9.41 95 9.54 29.46 2.48 0.0542 0.426 49)
+  ("table7" 3.98 14.6 19.22 84.01 0.77 0.0472 -0.718 27)
+  ("table8" 3.81 17.2 30.06 164.8 1.77 0.0086 0.671 13)
+  ("table9" 0.18 0.002 39.5 248.5 17.1 0.249 -6.5 3)
+)
+; 0: table name 1: diameter (table) 2: dynamic (table)
+; 3: table radius (AU) 4: table times (years)
+; 5: table Angle 6: table gnu hacker
+; 7: table (items) 8: objects
+)
+
+;; check list map string values guides document
+(set 'str-value1 '("str" 96000)))
+(set 'str-value2 '("str" {96000}))
+(set 'str-value3 '("str" [guides]96000[/guies]))     
+
+;; overview guides local series stream items button 18
+(set 's '("overview guides local series stream \n items button 18 to objects local cookies all."))
+
+;; println str list
+(println s)
+
+;; println newlisp decimal
+(println "\110\101\119\076\073\083\080")
+
+;; println newlisp hex
+(println "\x6e\x65\x77\x4c\x49\x53\x50"
+
+;; logic series films stream 	 
+(set 'films (read-file {/home/admin/ProjectEmacs/dk6hm/matrix/gnu/bin/sm4gnu.lisp}))
+
+;
+; length local films stream
+(utf8len (char 9600) +2)
+
+;; length local films stream
+(length (char 9600) +2)
+
+;; length local string
+(char 27)
+
+;; style method table link list
+(string '(sequence 1 96000) { produces } (sequence 1 96000) "\n")
+
+;; stability personal list tables
+(string '(sequence 1 96000) { produces } (sequence 1 96000) "\n")
+
+;; tables template values
+(set 'x 45)
+(string {the value of } 'x { is } x)
+
+;; tamplate series stream
+(dup 220 96000000)
+
+;; date local
+(date)
+
+
+
+~
---
+++ matrix/gnu/test/sm4gnu.lisp
@@ -0,0 +1,202 @@
+;; name project sm4gnu
+;; name program sm
+;; name extension newlisp
+;; name describer gnu
+;; name data 4
+;; name unit 4
+
+;; name oracle appach list check list button alhabet linear complex
+;; to matrix logical series stream key to local template link port
+;; type list key manager to select pass series stream message local
+;; script coffee.
+(set 'alphabet '("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k"
+"l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"))
+
+(13 alphabet)
+
+;; logic refrence of call
+(slice alphabet 13)
+
+;; type of reference ambiental
+(3 7 alphabet)
+
+;; type logical series stream
+(alphabet 3 7)
+
+;; logic of paremter type form until to analysis
+;; about connect the develop still memory build
+(set 'data (sequence 100 96000000))
+
+;; functional to needs running path dialog local develop
+;; static series stream method in ambient local list its
+(setf (data 5) 0)
+
+;; say series stream data popup check list ...
+data
+
+;; sequence check list series stream
+(setf (nth 5 (sequence 100 110)) 1)
+
+;; property select to dialog series stream
+(setf (nth 5 (set 's (sequence 100 110))) 1)
+
+
+;; strategy static series stream
+(set 'lst (sequence 0 96000000))
+
+;; active data
+lst
+
+;; strategy static series stream
+(set 'cnn (sequence 0 96000000))
+
+;; active data
+cnn
+
+;; strategy static series stream
+(set 'cnn-news (sequence 0 96000000))
+
+;; ative data
+cnn-news
+
+;; strategy static series stream
+(set 'cnn-brazil (sequence 0 96000000))
+
+;; active data
+cnn-brazil
+
+;; strategy static series stream
+(set 'glob (sequence 0 96000000))
+
+;; active data
+glob
+
+;; strategy static series stream
+(set 'globplay (sequence 0 96000000))
+
+;; active play
+globplay
+
+;; strategy static series stream
+(set 'globnews (sequence 0 96000000))
+
+;; active data
+globnews
+
+;; strategy static series stream
+(set 'band (sequence 0 96000000))
+
+;; active data
+band
+
+;; strategy static series stream
+(set 'bandnews (sequence 0 96000000))
+
+;; cative data
+bandnews
+
+;; strategy static series stream
+(set 'bandplay (sequence 0 96000000))
+
+;; active data
+bandplay
+
+;; strategy static series stream
+(set 'hope (sequence 0 96000000))
+
+;; active data
+hope
+
+;; strategy static series stream
+(set 'hope-new-time (sequence 0 96000000))
+
+;; active data
+hope-new-time
+
+;; strategy static series stream
+(set 'hope-angels (sequence 0 96000000))
+
+;; active data
+hope-angels
+
+;; strategy static series stream
+(set 'sbt (sequence 0 96000000))
+
+;; active data
+sbt
+
+;; strategy static series stream
+(set 'sbtplay (sequence 0 96000000))
+
+;; active data
+sbtplay
+
+;; strategy static series stream
+(set 'sbtnews (sequence 0 96000000))
+
+;; active data
+sbtnews
+
+
+;; dialog check list stream
+(set 'str "S2")
+
+;; server check list stream
+(setf (str 0) (char (inc (char $it))))
+
+;; active data delicious
+str
+
+;; server type logical until dialog say series stream
+(set 'data (sequence 1 96000000))
+
+;; data stream common newlisp
+(replace 5 data)
+
+;; mereding merendo ta merendado
+(set 'data '(("a" 1) ("b" 2)))
+
+;; mereding merendo ta merendado
+(set 'data (sequence 1 96000000))
+(replace 5 data 0)
+
+;; build 96000000 check list of pap cat
+(set 'data (sequence 1 96000000))
+
+;; items easy safes check list defence mereding
+(set 'data (sequence 1 10))
+
+;; logical series template need type static
+(replace 5 data 0 =)
+;; tables series stream static
+(set 'data (sequence 1 10))
+(replace 5 data 0)
+;-> (1 2 3 4 0 6 7 8 9 10)
+
+;; logic series tamplate data stream
+(set 'data (randomize (sequence 1 96000000)))
+
+;; logical pop static physics
+(set 'poptable '(
+("link1" 234 27 342 23 0)
+("link2" 92 0 239 47 134)
+("link3" 71 2 118 0)
+("link3" 10 14 58 12 )))
+
+;; logical series map type list
+(set 'l '((aaa 100) (bbb 200)))
+
+;; static tables sort
+(set-ref 200 l 300)
+
+;; logic static reference name objects
+(count '("a" "e" "i" "o" "u") (explode "the quick brown fox jumped over the lazy
+dog"))
+
+;; logic link stream type app
+(count (explode "aeiou") (explode "the quick brown fox jumped over the lazy
+dog"))
+
+;; logic editor bbedit common newlisp
+(set 'bbedit
+(directory ""))
